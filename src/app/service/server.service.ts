3007c8627480a79970b91aadbb1b818d032e17f8
Daniele410 <miraglia.daniele410@gmail.com>
2024-10-22 17:39:38 +0200

    fix problem service

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CustomResponse } from '../interface/custom-response';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
+import { CustomResponse } from '../interface/custom-response';
 
-import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Server } from '../interface/server';
 import { Status } from '../enum/status.enum';
 
@@ -11,20 +11,89 @@ import { Status } from '../enum/status.enum';
   providedIn: 'root'
 })
 export class ServerService {
-  private readonly apiUrl = 'http://localhost:8080';
+ 
+  private readonly apiUrl = 'any';
 
 
   constructor(private http: HttpClient) { }
 
-  servers$ = (): Observable<CustomResponse> =>
-    this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+  servers$ = (): Observable<CustomResponse> => {
+    return this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
       .pipe(
         tap(console.log),
         catchError(this.handleError)
       );
+  }
+
+  save$ = (server:Server): Observable<CustomResponse> => {
+    return this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+  }
+
+  ping$ = (ipAddress: string): Observable<CustomResponse> => {
+    return this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+  }
+
+  filter$ = (stutus: Status, response: CustomResponse): Observable<CustomResponse> => {
+    return new Observable<CustomResponse>(
+      subscriber => {
+        console.log(response);
+        subscriber.next(
+          status === Status.ALL ? { ...response, message: `Servers filtered by ${status} status` } :
+          {
+            ...response,
+            message: response.data.servers?.filter(server => server.status === status).length > 0 ? `Servers filtered by 
+            ${status === Status.SERVER_UP? 'SERVER UP' : 'SERVER DOWN' } status` : `No servers of ${status} found`,
+            data: { servers: response.data.servers
+              .filter(server => server.status === status) }
+          } 
+        );
+        subscriber.complete();
+      }
+    )
+    
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+  }
+
+  delete$ = (serverId: number): Observable<CustomResponse> => {
+    return this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+  }
+
+  handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError(() => `An error occurred - Error code: ${error.status}`);
+  }
+}
+
+/*   getServers(): Observable<CustomResponse>{
+    return this.http.get<CustomResponse>(`http://localhost:8080/server/list`);
+  } */
+
+    
+
+  /* servers$ = (): Observable<CustomResponse> =>
+    this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      ); */
 
 
-  save$ = (server: Server) => <Observable<CustomResponse>>
+ /*  save$ = (server: Server) => <Observable<CustomResponse>>
     this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
       .pipe(
         tap(console.log),
@@ -74,9 +143,10 @@ export class ServerService {
   private handleError(error: HttpErrorResponse): Observable<never> {
     console.log(error);
     return throwError(`An error occurred - Error code: ${error.status}`);
-  }
+  } */
 
   /* getServers(): Observable<CustomResponse> {
     return this.http.get<CustomResponse>('http://localhost:8080/server/list');
   } */
-}
+
+
